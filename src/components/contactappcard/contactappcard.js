e591838b13a419f4e4b11bced0fc64358d24a962
Hemant <hsgaikwad0015@gmail.com>
2023-10-15 10:26:42 +0530

    initial commit

---
+++ src/components/contactappcard/contactappcard.js
@@ -0,0 +1,22 @@
+import './contactappcard.css';
+import React from 'react';
+import imageedit from './edit button.png';
+import imagedel from './delete btn.png';
+export default function ContactAppCard({ name, mobile, email, deleteContact, enableEditMode, index }) {
+
+    return (
+        <div className='contactcard'>
+            <p className='contact-name'>👨 {name}</p>
+            <p className='contact-name'>📞 {mobile}</p>
+            <p className='contact-name'>✉ {email}</p>
+
+            <span onClick={() => {
+                deleteContact(mobile)
+            }}><img src={imagedel} className='deletebutton' /></span>
+
+            <span onClick={() => {
+                enableEditMode(index);
+            }}><img src={imageedit} className='editbtn' /></span>
+        </div>
+    )
+}
